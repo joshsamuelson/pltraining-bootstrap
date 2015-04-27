46cc3d09c21002dce2092a6947c23b94c510cce5
Josh Samuelson <js@puppetlabs.com>
2015-04-27 09:07:46 -0700

    DHCP settings for skytap vms

---
+++ manifests/network_settings.pp
@@ -0,0 +1,16 @@
+# Set DHClient to be robust on skytap
+# Per: http://help.skytap.com/#Linux_VM_Connectivity_Issues.html
+
+class bootstrap::network_settings {
+  file_line { 'DHCLIENTARGS':
+    path    => '/etc/sysconfig/network-scripts/ifcfg-eth0',
+    line    => 'DHCLIENTARGS="-nw"',
+    matches => '^DHCLIENTARGS.*$',
+  }
+  file_line { 'PERSISTENT_DHCLIENT':
+    path    => '/etc/sysconfic/network-scripts/ifcfg-eth0',
+    line    => 'PERSISTENT_DHCLIENT=yes',
+    matches => '^PERSISTENT_DHCLIENT.*$',
+  }
+}
+
