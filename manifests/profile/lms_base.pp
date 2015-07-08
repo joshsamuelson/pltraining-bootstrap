e150db5d653427e25f02d2b2415823c26ad33b19
Josh Samuelson <js@puppetlabs.com>
2015-07-08 15:19:49 -0700

    We don't need to logout the VMs with skytap

--- manifests/profile/lms_base.pp
+++ manifests/profile/lms_base.pp
@@ -1,8 +1,4 @@
 class bootstrap::profile::lms_base {
-  file { '/etc/bash.bash_logout':
-    ensure => present,
-    source => 'puppet:///modules/bootstrap/lms/bash.bash_logout',
-  }
   file {'/etc/profile.d/profile.sh':
     ensure => present,
     mode   => 755,
